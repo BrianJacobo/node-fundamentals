207a200fd09609898d17d4ef0df54f425fe1729c
Brian Jacobo Fuentes <brianjacobof@pop-os.localdomain>
2021-07-01 16:30:30 -0500

    conceptos

---
+++ 01-conceptos/monohilo.js
@@ -0,0 +1,12 @@
+console.log("haciendo prueba de monohilo");
+
+let i = 0
+setInterval(function(){
+    console.log(i)
+    i++
+    if(i == 7){
+        var a = 3 + z
+    }
+},1000)
+
+console.log("hola soy la linea 12");
