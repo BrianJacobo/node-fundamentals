37b6424a9a74182f823b20d3caf5431a5699f33a
Brian Jacobo Fuentes <jacobo.brian.fuentes@outlook.com>
2021-07-12 16:46:53 -0500

    sharp para imagenes grayscale

--- 09-comon-modules/sharp.js
+++ 09-comon-modules/sharp.js
@@ -1,3 +1,7 @@
 const sharp = require('sharp')
 
+// modificar el tamaño de la imagen
 sharp('./javascript-3.jpg').resize(380).toFile('resize.png')
+
+// A escala de grises
+sharp('./javascript-3.jpg').resize(380).graysacle().toFile('resize.png')
