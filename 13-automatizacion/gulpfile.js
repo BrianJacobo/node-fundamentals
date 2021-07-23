16d3fb3b47fbd02579b9bfaf2f97a98a11ba21b4
Brian Jacobo Fuentes <jacobo.brian.fuentes@outlook.com>
2021-07-23 01:30:17 -0500

    stand by

---
+++ 13-automatizacion/gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp')
+
+gulp.task('build',function(cb){
+    console.log('Construyendo el sitio');
+    setTimeout(cb,1200)
+})
+
+gulps.task('serve',function(cb){
+    
+})
